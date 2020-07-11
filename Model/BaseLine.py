4b8e0f6a946da267a8ed0b7dde4a204c961e43b6
kaku0o0 <43189700+kaku0o0@users.noreply.github.com>
2020-07-11 01:38:48 -0400

    Update BaseLine.py
    Added randomtreee forest

--- Model/BaseLine.py
+++ Model/BaseLine.py
@@ -5,7 +5,6 @@ import random
 import torch.utils.data
 import time
 import os
-import io
 import torchvision
 from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
@@ -13,13 +12,30 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import Dataset
 import matplotlib.pyplot as plt
 import pandas as pd
 import cv2
 from PIL import Image
 from torchvision.transforms import ToTensor, ToPILImage, Normalize, Compose
 from torch.utils.data import DataLoader
+import numpy as np
+import random
+import scipy.io
+import io
+from torch.utils.data import Dataset
+import torch
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+
+# evaluate random forest algorithm for classification
+from numpy import mean
+from numpy import std
+from sklearn.datasets import make_classification
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestRegressor
 
 # Reference: https://stackoverflow.com/questions/56696147/pytorch-how-to-create-a-custom-dataset-with-reference-table
 
@@ -297,17 +313,66 @@ if __name__ == '__main__':
     
     images, labels = dataiter.next()
     images = images.numpy() # convert images to numpy for display
+    clf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+    print(labels)
+    print(labels.shape)
+    print(images.shape)
+    new_img=images[:,:,:,0:20]
+    print(new_img.shape)
+    batch,color, nx, ny = images.shape
+    images2=images.reshape((batch, color*nx*ny))
+    batch, lx, ly = labels.shape
+    labels2=labels.reshape((batch, lx*ly))
+    print(labels2)
 
+    clf.fit(images2,labels2)
+    pred=clf.predict(images2)
+    print(pred)
+    sep_pre=pred.reshape((batch, lx,ly))
+    my_array = np.zeros([batch, lx])
+    my_array2 = np.zeros([batch, lx])
+    for i in range(batch) :
+        for k in range(lx):
+            my_array[i][k]=np.argmax(sep_pre[i][k])
+            my_array2[i][k]=np.argmax(labels[i][k])
+    print (my_array)
+    print (my_array2)
+   # print(np.argmax(pred.reshape((batch, lx,ly))))
+    errors=abs(my_array2 - my_array)
+    print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
+    #print(accuracy_score(my_array2,my_array))
+    
+    images, labels = dataiter.next()
+    batch,color, nx, ny = images.shape
+    images2=images.reshape((batch, color*nx*ny))
+    batch, lx, ly = labels.shape
+    labels2=labels.reshape((batch, lx*ly))
+    print(labels2)
+
+    pred=clf.predict(images2)
+    print(pred)
+    sep_pre=pred.reshape((batch, lx,ly))
+    my_array = np.zeros([batch, lx])
+    my_array2 = np.zeros([batch, lx])
+    for i in range(batch) :
+        for k in range(lx):
+            my_array[i][k]=np.argmax(sep_pre[i][k])
+            my_array2[i][k]=np.argmax(labels[i][k])
+    print (my_array)
+    print (my_array2)
+   # print(np.argmax(pred.reshape((batch, lx,ly))))
+    errors=abs(my_array2 - my_array)
+    print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
     # plot the images in the batch, along with the corresponding labels
     count = 0
     while(1 == 1):
         if (count==10): break
         print(labels[count])
 
-        cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-        cv2.imshow('image', np.transpose(images[count], (1, 2, 0)))
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        #cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+        #cv2.imshow('image', np.transpose(images[count], (1, 2, 0)))
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
         count+=1
 
 
